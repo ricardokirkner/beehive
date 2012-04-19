aae004859ec5f0a38b96814addfe555d4db63d12
Ricardo Kirkner <ricardo@kirkner.com.ar>
2012-04-18 22:36:05 -0300

    initial skeleton

---
+++ src/beehive/__init__.py
@@ -0,0 +1,9 @@
+"""beehive package"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__metaclass__ = type
+__all__ = [
+    ]
+
+__version__ = '0.0.1'
---
+++ template.py
@@ -0,0 +1,7 @@
+"""Template for python modules"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+__metaclass__ = type
+__all__ = [
+    ]
